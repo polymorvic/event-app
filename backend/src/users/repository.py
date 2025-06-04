97a5dee6162f4804dd30985c121c0d718fda08a0
polymorvic <polymorphic.bite@gmail.com>
2025-06-04 19:15:33 +0200

    add password hashing functionality

--- backend/src/users/repository.py
+++ backend/src/users/repository.py
@@ -3,7 +3,10 @@ from sqlalchemy import select
 from fastapi import HTTPException
 from src.users.models import User
 from src.users.schemas import UserIn
+from passlib.context import CryptContext
 
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def user_create(user_in: UserIn, dbs: Session) -> User:
     db_user = dbs.execute(
@@ -13,7 +16,9 @@ def user_create(user_in: UserIn, dbs: Session) -> User:
         raise HTTPException(
             status_code=400, detail="User with given email already exists"
         )
+    hashed_password = pwd_context.hash(user_in.password)
     user_data = user_in.model_dump()
+    user_data["password"] = hashed_password
     user = User(**user_data)
     dbs.add(user)
     dbs.commit()
