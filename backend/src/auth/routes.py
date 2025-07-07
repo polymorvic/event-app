ad4201bcb2fd88abe7b3c319b9183c600ed3dd44
polymorvic <polymorphic.bite@gmail.com>
2025-07-07 18:29:14 +0200

    check if user account is activated by logging

--- backend/src/auth/routes.py
+++ backend/src/auth/routes.py
@@ -23,6 +23,11 @@ async def login_for_access_token(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    if not user.is_activated:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not activated, please check your email",
+        )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
