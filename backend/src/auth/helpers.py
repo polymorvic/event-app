5cf4541f48424fd67a6bfd9e1449809526b87c03
polymorvic <polymorphic.bite@gmail.com>
2025-07-22 18:41:56 +0200

    change user password encryption

--- backend/src/auth/helpers.py
+++ backend/src/auth/helpers.py
@@ -1,21 +1,21 @@
 from src.auth.repository import get_user_by_email
-from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from src.users.models import User
+import bcrypt
 from datetime import datetime, timedelta, timezone
 import jwt
 from src import secrets
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 ALGORITHM = "HS256"
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 
-def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+def get_password_hash(password):
+    hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt(rounds=12))
+    return hashed_password.decode('utf-8')
 
 
 def authenticate_user(dbs: Session, email: str, password: str) -> User | None:
