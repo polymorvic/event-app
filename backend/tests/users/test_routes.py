246a6a4d7b18ce925683b83381fd5d3f7a400419
polymorvic <polymorphic.bite@gmail.com>
2025-08-26 19:11:17 +0200

    add user create endpoint

--- backend/tests/users/test_routes.py
+++ backend/tests/users/test_routes.py
@@ -2,29 +2,37 @@ from fastapi.testclient import TestClient
 from src.api.routes import app
 from unittest.mock import patch
 from src.users.models import User
+from src.auth.helpers import generate_verification_token
 
-import pytest
+
 
 client = TestClient(app)
 
-
-# @pytest.mark.asyncio
-# @patch("src.mail.service.send_verification_mail")
-def test_creating_user(db_session):
-    # assert db_session.query(Users).count() == 0
+@patch("src.users.routes.send_verification_mail")
+def test_creating_user(send_verification_mail, db_session):
+    
+    send_verification_mail.return_value = None
+    
+    assert db_session.query(User).count() == 0
 
     response = client.post(
         "/users/register",
         json={
-            "email": "abc@wp.pl",
+            "email": "przemyslawmarkiewicz97@gmail.com",
             "first_name": "Test 123",
             "last_name": "Test Name 123 vbn",
             "password": "qwerty123",
         },
     )
+    
+    assert db_session.query(User).count() == 1
+    user = db_session.query(User).first()
+    
+    assert user
+    
+    email_token = generate_verification_token(user.id, user.email)
+    
+    send_verification_mail.assert_called_once_with(user.email, email_token)
 
-    breakpoint()
-
-    # assert response.status_code == 200
-    # assert db_session.query(Users).count() == 1
-    # send_verification_mail.assert_called_once()
+    assert response.status_code == 200
+    assert db_session.query(User).count() == 1
