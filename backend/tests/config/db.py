b9b18aa9b9a19d646153578f44bbcd407bc1640c
polymorvic <polymorphic.bite@gmail.com>
2025-08-26 18:21:23 +0200

    setup test database

--- backend/tests/config/db.py
+++ backend/tests/config/db.py
@@ -1,29 +1,77 @@
 from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
+from sqlalchemy.orm import sessionmaker
 from src.config import database_name
-from typing import Generator
-
+from src.db.connection import BaseModel
 from src import config, secrets
 import pytest
-
+from contextlib import contextmanager
+from src import db
 
 def test_database_name():
     return database_name() + "_test"
 
 
-@pytest.fixture()
-def get_test_database():
+@pytest.fixture(scope="session")
+def create_test_database():
     engine = create_engine(
-        "postgresql+psycopg://{user}:{password}@{host}:{port}/{database_name}".format(
+        "postgresql+psycopg://{user}:{password}@{host}:{port}".format(
+            user=secrets.database_user(),
             host=config.database_host(),
             port=config.database_port(),
-            database_name=test_database_name,
-            user=secrets.database_user(),
             password=secrets.database_password(),
-        )
+        ),
+        isolation_level="AUTOCOMMIT",
     )
     
     with engine.connect() as connection:
-        connection.execute(text("DROP DATABASE IF EXISTS {db_name}".format(db_name=test_database_name))) 
-        connection.execute(text("CREATE DATABASE {db_name}".format(db_name=test_database_name))) 
+        connection.execute(text("DROP DATABASE IF EXISTS {db_name}".format(db_name=test_database_name()))) 
+        connection.execute(text("CREATE DATABASE {db_name}".format(db_name=test_database_name()))) 
     engine.dispose()
+
+
+
+@pytest.fixture(scope="session")
+def database_engine(create_test_database):
+    return create_engine(
+        "postgresql+psycopg://{user}:{password}@{host}:{port}/{database}".format(
+            user="postgres",
+            password="postgres",
+            host=config.database_host(),
+            port=config.database_port(),
+            database=test_database_name()
+        )
+    )
+
+
+
+@pytest.fixture(scope="session")
+def connection(database_engine):
+    with database_engine.connect() as connection:
+        with connection.begin():
+            BaseModel.metadata.create_all(connection)
+            connection.execute(text("CREATE EXTENSION pg_trgm;"))
+    
+        yield connection
+        
+    connection.close()
+    
+    
+@pytest.fixture
+def db_session(connection, monkeypatch):
+    transactoin = connection.begin()
+    session_factory = sessionmaker(bind=connection)
+    
+    @contextmanager
+    def session_factory_context_manager():
+        connection.begin_nested()
+        yield session_factory()
+        
+    monkeypatch.setattr(
+        db.connection,
+        "_session",
+        session_factory_context_manager
+    )
+    
+    session = session_factory()
+    yield session
+    transactoin.rollback()
--- backend/tests/users/test_routes.py
+++ backend/tests/users/test_routes.py
@@ -1,18 +1,28 @@
 from fastapi.testclient import TestClient
 from src.api.routes import app
 from unittest.mock import patch
+from src.users.models import User
+
 import pytest
 
 client = TestClient(app)
 
-@pytest.mark.asyncio
-@patch("src.mail.service.send_verification_mail")
-async def test_creating_user(send_verification_mail):
+# @pytest.mark.asyncio
+# @patch("src.mail.service.send_verification_mail")
+def test_creating_user(db_session):
+    
+    # assert db_session.query(Users).count() == 0
+    
     response = client.post("/users/register", json={
-        "email": "ssss@wp.pl",
-        "first_name": 'Test',
-        "last_name": 'Test Name',
+        "email": "abc@wp.pl",
+        "first_name": 'Test 123',
+        "last_name": 'Test Name 123 vbn',
         "password": 'qwerty123',
     })
     
-    send_verification_mail.assert_called_once()
+    breakpoint()
+    
+    
+    # assert response.status_code == 200
+    # assert db_session.query(Users).count() == 1
+    # send_verification_mail.assert_called_once()
