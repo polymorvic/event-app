dacc023ccd0a34cb58c3ef9441d8fe675112e0d6
polymorvic <polymorphic.bite@gmail.com>
2025-09-08 16:07:23 +0200

    add test_user_activate

--- backend/tests/users/test_routes.py
+++ backend/tests/users/test_routes.py
@@ -15,7 +15,7 @@ def test_creating_user(send_verification_mail, db_session):
     response = client.post(
         "/users/register",
         json={
-            "email": "przemyslawmarkiewicz97@gmail.com",
+            "email": "test_mail@gmail.com",
             "first_name": "Test 123",
             "last_name": "Test Name 123 vbn",
             "password": "qwerty123",
@@ -33,3 +33,21 @@ def test_creating_user(send_verification_mail, db_session):
 
     assert response.status_code == 200
     assert db_session.query(User).count() == 1
+
+
+def test_user_activate(db_session, user_factory):
+    
+    user = user_factory(email="testmail@gmail.com", is_activated=False)
+    
+    assert user.is_activated == False
+    
+    email_token = generate_verification_token(user.id, user.email)
+    
+
+    response = client.get(f"/auth/verify-email/{email_token}")
+    
+    db_session.refresh(user)
+
+    assert user.is_activated == True
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Account for user testmail@gmail.com has been activated successfully.'}
