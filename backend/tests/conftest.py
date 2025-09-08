56a28c9b9d6bb4a1f5a418ba56959a76cffceeda
polymorvic <polymorphic.bite@gmail.com>
2025-09-08 16:57:57 +0200

    add users/me endpoint testing

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,2 +1,3 @@
 from tests.config.db import *
 from tests.factories.user_factory import *
+from tests.fixtures import *
--- backend/tests/users/test_routes.py
+++ backend/tests/users/test_routes.py
@@ -1,8 +1,12 @@
+from datetime import datetime
 from fastapi.testclient import TestClient
 from src.api.routes import app
 from unittest.mock import patch
 from src.users.models import User
 from src.auth.helpers import generate_verification_token
+from src.auth.dependencies import authentication
+from src.auth.helpers import create_access_token
+
 
 
 
@@ -51,3 +55,25 @@ def test_user_activate(db_session, user_factory):
     assert user.is_activated == True
     assert response.status_code == 200
     assert response.json() == {'message': 'Account for user testmail@gmail.com has been activated successfully.'}
+
+
+def test_users_me_without_dependency_override(user_factory):
+    
+    test_user = user_factory()
+    token = create_access_token(data={"sub": test_user.email})
+    response = client.get("/users/me", headers={"Authorization": f"Bearer {token}"})
+    
+    assert response.status_code == 200
+    
+    assert response.json()['email'] == test_user.email
+
+
+
+def test_users_me_with_dependency_override(authenticated_user):
+    response = client.get("/users/me")
+    
+    assert response.status_code == 200
+    
+    assert response.json()['email'] == authenticated_user.email
+
+    
